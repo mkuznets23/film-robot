3f15513edd48f7518690076bf9a0b1c9c43c0bf6
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-01-05 02:51:58 -0800

    first additions to eye_screen

---
+++ eye_screen.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#import chardet
+import os
+import sys 
+import time
+import logging
+import spidev as SPI
+sys.path.append("..")
+# from lib import LCD_1inch28
+from PIL import Image,ImageDraw,ImageFont
+
+# Raspberry Pi pin configuration:
+RST = 27
+DC = 25
+BL = 18
+bus = 0 
+device = 0 
+logging.basicConfig(level=logging.DEBUG)
+try:
+    # display with hardware SPI:
+    ''' Warning!!!Don't  creation of multiple displayer objects!!! '''
+    #disp = LCD_1inch28.LCD_1inch28(spi=SPI.SpiDev(bus, device),spi_freq=10000000,rst=RST,dc=DC,bl=BL)
+    disp = LCD_1inch28.LCD_1inch28()
+    # Initialize library.
+    disp.Init()
+    # Clear display.
+    disp.clear()
+    #Set the backlight to 100
+    disp.bl_DutyCycle(50)
+
+    with Image.open('circle_resized.gif') as im:
+        im.seek(1)  # skip to the second frame
+
+        try:
+            while 1:
+                im.seek(im.tell() + 1)
+                im_r=im.rotate(180)
+                disp.ShowImage(im_r)
+                time.sleep(0.01)
+                # do something to im
+        except EOFError:
+            pass  # end of sequence
+    
+    time.sleep(3)
+    disp.module_exit()
+    logging.info("quit:")
+except IOError as e:
+    logging.info(e)    
+except KeyboardInterrupt:
+    disp.module_exit()
+    logging.info("quit:")
+    exit()
