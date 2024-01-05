65f8b16716d85de438acf399ad9241e27f8b74d3
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-01-05 02:55:30 -0800

    uncomment "from lib import LCD_1inch28"

--- eye_screen.py
+++ eye_screen.py
@@ -7,7 +7,7 @@ import time
 import logging
 import spidev as SPI
 sys.path.append("..")
-# from lib import LCD_1inch28
+from lib import LCD_1inch28
 from PIL import Image,ImageDraw,ImageFont
 
 # Raspberry Pi pin configuration:
