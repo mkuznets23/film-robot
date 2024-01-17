870928b0183b6b999a7357c7e31cc31dac076818
mkuznets23 <77693398+mkuznets23@users.noreply.github.com>
2024-01-16 17:10:42 -0800

    showing im and not im_r

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def thread_eye_show():
         try:
             im.seek(im.tell() + 1)
             # im_r=im.rotate(180)
-            disp.ShowImage(im_r)
+            disp.ShowImage(im)
             time.sleep(0.01)
             # do something to im
         except EOFError:
